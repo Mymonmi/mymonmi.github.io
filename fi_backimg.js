b660ed833d42175802f0227c251b0d234b2ca5c1
Mymonmi <141899162+Mymonmi@users.noreply.github.com>
2023-11-12 20:29:07 +0900

    Update fi_backimg.js

--- fi_backimg.js
+++ fi_backimg.js
@@ -1,5 +1,5 @@
 const images = [...new Array(5).keys()].map(
-  (val, idx) => String(val + 1) + ".jpeg"
+  (val, idx) => String(val + 1) + ".jpg"
 );
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
